6c0345b8342406937f0a3682679196341e229c4a
Osama Azhar <osamaazhar.edu@gmail.com>
2021-02-24 21:57:58 +0500

    Initial Routing Of Pages Performed.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+import React from 'react'
 
-function App() {
+
+import {Route, Switch} from 'react-router-dom';
+
+import Home from './Pages/Home/Home'
+import Authentication from './Pages/Authentication/Authentication'
+import Category from './Pages/Category/Category'
+import Checkout from './Pages/Checkout/Checkout'
+import CategoryProducts from './Pages/CategoryProducts/CategoryProducts'
+
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>REact app</h1>
+    <div>
+      <Switch>
+        <Route path="/" component={Home} exact/>
+        <Route path="/authentication" component={Authentication} />
+        <Route path="/categories" component={Category}/>
+        <Route path="/category-products" component={CategoryProducts}/>
+        <Route path="/checkout" component={Checkout}/>
+
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+
+
+export default App
---
+++ src/Pages/CategoryProducts/CategoryProducts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryProducts = () => {
+    return (
+        <div>
+            <h1>CategoryProductsCategoryProducts</h1>
+        </div>
+    )
+}
+
+export default CategoryProducts
