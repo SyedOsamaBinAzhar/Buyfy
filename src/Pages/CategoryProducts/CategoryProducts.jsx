d4d85e351bc53bf74a3087f2b2423b307619f69b
Osama Azhar <osamaazhar.edu@gmail.com>
2021-03-10 05:57:02 +0500

    done with category product page styling.

--- src/Pages/CategoryProducts/CategoryProducts.jsx
+++ src/Pages/CategoryProducts/CategoryProducts.jsx
@@ -2,6 +2,9 @@ import React, {useEffect} from 'react'
 import {connect} from "react-redux";
 import { fetchCategoryProducts, clearProducts } from '../../Redux/Products/ProductsAction';
 import ProductCard from "../../Components/ProductCard/ProductCard"
+import "./CategoryProducts.css"
+
+
 const CategoryProducts = ({match:{params: {category}},fetchCategoryProducts,products,clearProducts}) => {
 
     useEffect(() => {
@@ -15,9 +18,12 @@ const CategoryProducts = ({match:{params: {category}},fetchCategoryProducts,prod
     // console.log(products)
     
     return (
-        <div>
-            <h1>{category} Products Page</h1>
+        <div className="categoryProducts">
+            <div className="category_product_list">
+            {/* <h1>{category} Products Page</h1> */}
             {products.map((product) => <ProductCard key={product.title} {...product}/>)}
+
+            </div>
         </div>
     )
 }
